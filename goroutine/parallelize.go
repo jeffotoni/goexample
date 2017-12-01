6e1c12516890ed3f2ebab4ab0a7a35a6dbfa7aa0
jefferson otoni lima <jeff.otoni@gmail.com>
2017-12-01 14:48:10 -0200

    new example parallelism

---
+++ goroutine/parallelize.go
@@ -0,0 +1,64 @@
+/*
+* Example parallelism
+*
+* @package     main
+* @author      @jeffotoni
+* @size        01/08/2017
+*
+ */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// Parallelize parallelizes the function calls
+func Parallelize(functions ...func()) {
+
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(functions))
+
+	defer waitGroup.Wait()
+
+	for _, function := range functions {
+		go func(copy func()) {
+			defer waitGroup.Done()
+			copy()
+		}(function)
+	}
+}
+
+func dowork(goroutine, loopindex int) {
+	// simulate work
+	time.Sleep(time.Second * time.Duration(5))
+	fmt.Printf("gr[%d]: i=%d\n", goroutine, loopindex)
+}
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		dowork(n, i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func main() {
+
+	func1 := func() {
+		f(0)
+	}
+
+	func2 := func() {
+		f(1)
+	}
+
+	func3 := func() {
+		f(2)
+	}
+
+	Parallelize(func1, func2, func3)
+}
