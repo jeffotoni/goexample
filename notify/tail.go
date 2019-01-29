37bf9e48ee264500207e2c167f2f40dc0f93933a
jefferson <jeff.otoni@gmail.com>
2019-01-29 02:11:13 -0200

    tail in go

--- notify/tail.go
+++ notify/tail.go
@@ -18,39 +18,84 @@ import (
 	"time"
 )
 
-func readFile(file io.Reader) error {
-	r := bufio.NewReader(file)
-	for {
-		by, err := r.ReadBytes('\n')
-		if err != nil && err != io.EOF {
-			return err
-		}
-		fmt.Print(string(by))
-		if err == io.EOF {
-			time.Sleep(time.Second)
-		}
-	}
-}
+var tf = flag.String("file", "", "example: file.txt")
+var r *bufio.Reader
 
-func main() {
-	tf := flag.String("file", "", "example: file.txt")
+func init() {
 	flag.Parse()
-	if len(os.Args) < 3 {
+	if len(flag.Args()) != 0 {
 		flag.PrintDefaults()
 		return
 	}
+}
 
+func WatchFile() {
 	if existsFile(*tf) {
 		file, err := os.Open(*tf) // For read access.
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		readFile(file)
 	} else {
 		fmt.Println("File not exist!")
 		flag.PrintDefaults()
 	}
+}
+
+func tryToOpen() {
+	if existsFile(*tf) {
+		file, err := os.Open(*tf) // For read access.
+		if err != nil {
+			log.Fatal(err)
+		}
+		// r global
+		r = bufio.NewReader(file)
+	}
+}
+
+func readFile(file io.Reader) error {
+
+	// r global
+	r = bufio.NewReader(file)
+
+	go func() error {
+		for {
+			by, err := r.ReadBytes('\n')
+			if err != nil && err != io.EOF {
+				return err
+			}
+			fmt.Print(string(by))
+			if err == io.EOF {
+				time.Sleep(time.Second * 2)
+			}
+		}
+	}()
+
+	// read file
+	go func() {
+		for {
+			time.Sleep(time.Second * 1)
+			//println("test file..")
+			existgof()
+		}
+	}()
+
+	for {
+	}
+	return nil
+}
+
+func existgof() {
+	if !existsFile(*tf) {
+		// not exist
+		// try in 10s recursive
+		time.Sleep(time.Second * 10)
+		tryToOpen()
+	}
+}
+
+func main() {
+	WatchFile()
 }
 
 func existsFile(name string) bool {
