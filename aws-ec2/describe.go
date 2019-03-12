b83b1c63123d280c35f03511a4c686e396a338a2
jefferson <jeff.otoni@gmail.com>
2019-03-12 20:57:36 -0300

    aws ec2, describe

---
+++ aws-ec2/describe.go
@@ -0,0 +1,63 @@
+// Go in action
+// @jeffotoni
+// 2019-01-16
+
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
+)
+
+// define flags
+var (
+    flagRegion   = flag.String("region", "", "example: us-east-1")
+    flagInstance = flag.String("instance", "", "example: i-05bef719c14d68d10")
+)
+
+// init
+func init() {
+    flag.Parse()
+    if *flagRegion == "" {
+        flag.PrintDefaults()
+        os.Exit(0)
+    }
+
+    if *flagInstance == "" {
+        flag.PrintDefaults()
+        os.Exit(0)
+    }
+}
+
+func main() {
+    // session aws create
+    svc := ec2.New(session.New(), aws.NewConfig().WithRegion(*flagRegion))
+    input := &ec2.DescribeInstancesInput{
+        InstanceIds: []*string{
+            aws.String(*flagInstance),
+        },
+    }
+
+    result, err := svc.DescribeInstances(input)
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok {
+            switch aerr.Code() {
+            default:
+                fmt.Println(aerr.Error())
+            }
+        } else {
+            // Print the error, cast err to awserr.Error to get the Code and
+            // Message from an error.
+            fmt.Println(err.Error())
+        }
+        return
+    }
+
+    fmt.Println(result)
+}
