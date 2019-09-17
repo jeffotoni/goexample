7142f02ccb0a79d45eb0d47ca09e94117d8ad7cf
jefferson <jeff.otoni@gmail.com>
2019-09-17 11:07:01 -0300

    update em lambda size object

--- lambdas/size-file-s3/README.md
+++ lambdas/size-file-s3/README.md
@@ -2,14 +2,13 @@
 
 Este lambda foi desenvolvido em Go então para que possa compilar você precisa instalar em sua máquina mais detalhes aqui: [golang.org](https://golang.org) é muito simples instalar e usa-lo.
 
-Este lambda irá escutar uma bucket S3 abrir o arquivo, se o tamanho do arquivo não for o que você definiu ele excluir o arquivo do bucket.
+Caso não queira instalar tem como fazer tudo via docker, [golang docker](https://github.com/jeffotoni/goworkshopdevops/blob/master/README.md#installation-docker)
 
+Este lambda irá escutar uma bucket S3 receber os dados do arquivo e se o tamanho do arquivo não for o que você definiu ele excluir o arquivo do bucket.
 Edita o sizeobject.go na linha 18 SIZE, lá basta subistituir o valor da variável para o tamanho que deseja em bytes.
-
 Para tudo funcionar corretamente você precisa criar o lambda lá na AWS e depois criar a politica para acesso ao seu bucket pelo lambda, tem um arquivo [funcao-acl-politica-bucket.acl] de exemplo da política de acesso você pode utiliza-lo como exemplo e melhorar conforme sua necessidade.
 
 Deixei um Makefile para ajudar, para para rodar make deploy você precisa do awscli instalado e permissão para executa-lo.
-
 
 ## make build
 Somente build, não há necessidade de awscli, ele compila o programa e gera o zip para que você possa enviar para AWS LAMBDA manualmente.
@@ -23,9 +22,7 @@ $ make build
 ## make deploy
 Este comando ele faz build e envia para S3 e depois atualiza sua função Lambda dinâmicamente, vc não precisa entrar no console aws para fazer isto este deploy faz isto para você.
 Mas para isto funcionar corretamente vc precisa configurar suas credenciais corretamente em sua máquina e instalar o awscli.
-
 Abre o Makefile e configura as variaveis LAMBADA_NOME e BUCKET_DEPLOY que é o nome do Bucket que deseja jogar o zip para fazer deploy.
-
 Assim que tudo finalizar ele remove o arquivo zip e binário gerado de seu diretório.
 
 ```bash
--- lambdas/size-file-s3/sizeobject.go
+++ lambdas/size-file-s3/sizeobject.go
@@ -9,7 +9,6 @@ import (
     "github.com/aws/aws-lambda-go/events"
     "github.com/aws/aws-lambda-go/lambda"
     "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/awserr"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/s3"
     "log"
@@ -27,30 +26,10 @@ func handlerS3(ctx context.Context, s3Event events.S3Event) (MsgResponse, error)
     for _, record := range s3Event.Records {
 
         rs3 := record.S3
-        log.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, rs3.Bucket.Name, rs3.Object.Key)
+        log.Printf("[%s - %s] Bucket = %s, Key = %s Size = %d\n", record.EventSource, record.EventTime, rs3.Bucket.Name, rs3.Object.Key, rs3.Object.Size)
 
-        input := &s3.GetObjectInput{
-            Bucket: aws.String(rs3.Bucket.Name),
-            Key:    aws.String(rs3.Object.Key),
-        }
-
-        svc := s3.New(session.New())
-        result, err := svc.GetObject(input)
-        if err != nil {
-            if aerr, ok := err.(awserr.Error); ok {
-                switch aerr.Code() {
-                case s3.ErrCodeNoSuchKey:
-                    log.Println(s3.ErrCodeNoSuchKey, aerr.Error())
-                default:
-                    log.Println(aerr.Error())
-                }
-            } else {
-                log.Println(err.Error())
-            }
-            continue
-        }
-
-        if aws.Int64Value(result.ContentLength) > SIZE {
+        if rs3.Object.Size > SIZE {
+            svc := s3.New(session.New())
             _, err := svc.DeleteObject(&s3.DeleteObjectInput{
                 Bucket: aws.String(rs3.Bucket.Name),
                 Key:    aws.String(rs3.Object.Key)})
@@ -60,7 +39,6 @@ func handlerS3(ctx context.Context, s3Event events.S3Event) (MsgResponse, error)
             }
 
             log.Println("Arquivo maior que o permitido: key: ", rs3.Object.Key, "excluido com [sucesso]")
-
         }
     }
 
