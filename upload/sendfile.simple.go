ac8475b96fb6cc41e11a7b9a8135b9c311e72ed3
jefferson <jeff.otoni@gmail.com>
2019-11-29 18:45:42 -0300

    complet part send file

--- upload/sendfile.simple.go
+++ upload/sendfile.simple.go
@@ -3,11 +3,13 @@ package main
 import (
   "bytes"
   "io"
+  "io/ioutil"
+  "log"
   "mime/multipart"
   "net/http"
+  "net/textproto"
   "os"
   "path"
-  "path/filepath"
 )
 
 func main() {
@@ -20,12 +22,31 @@ func main() {
 
   body := &bytes.Buffer{}
   writer := multipart.NewWriter(body)
-  part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+  //part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+
+  mh := make(textproto.MIMEHeader)
+  mh.Set("Content-Type", "image/png")
+  mh.Set("Content-Disposition", "form-data; name=\"file\"; filename=\"jeff.png\"")
+  part, err := writer.CreatePart(mh)
+  if nil != err {
+    panic(err.Error())
+    return
+  }
+
   io.Copy(part, file)
   writer.Close()
 
   r, _ := http.NewRequest("POST", "http://localhost:8282", body)
   r.Header.Add("Content-Type", writer.FormDataContentType())
+
   client := &http.Client{}
-  client.Do(r)
+  resp, err := client.Do(r)
+  if err != nil {
+    log.Println(err.Error())
+    return
+  }
+  defer resp.Body.Close()
+  bodyMsg, _ := ioutil.ReadAll(resp.Body)
+  log.Println(string(bodyMsg))
+
 }
