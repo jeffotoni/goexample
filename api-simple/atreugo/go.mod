56f0469b964824c7d1937c8b2d66cf374af81976
jefferson <jeff.otoni@gmail.com>
2020-01-11 09:04:43 -0300

    api simple atreugo

---
+++ api-simple/atreugo/go.mod
@@ -0,0 +1,5 @@
+module atreugo
+
+go 1.13
+
+require github.com/savsgio/atreugo/v10 v10.0.0 // indirect
---
+++ api-simple/atreugo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/savsgio/atreugo/v10"
+)
+
+func main() {
+	config := &atreugo.Config{
+		Addr: "0.0.0.0:8000",
+	}
+	server := atreugo.New(config)
+
+	// Register a route
+	server.Path("GET", "/api/user", func(ctx *atreugo.RequestCtx) error {
+		return ctx.TextResponse("GET /api/user")
+	})
+
+	server.Path("POST", "/api/user", func(ctx *atreugo.RequestCtx) error {
+		type t struct {
+			Nome  string `json:"nome"`
+			Login string `json:login`
+		}
+		var v = t{"jefferson", "jeffotoni"}
+		return ctx.JSONResponse(v)
+	})
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
