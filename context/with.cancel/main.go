379956529dae5452a08c0d9d76ff69b3dcfb85b8
jefferson <jeff.otoni@gmail.com>
2020-04-17 14:33:31 -0300

    struct mutext

--- context/with.cancel/main.go
+++ context/with.cancel/main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
+type Result struct {
+	Body string
+	sync.Mutex
+}
+
 func main() {
+
+	var r Result
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	go f1(6, 1, cancel)
-	go f1(2, 2, cancel)
-	go f1(1, 3, cancel)
+	go f1(6, 1, cancel, &r)
+	go f1(2, 2, cancel, &r)
+	go f1(1, 3, cancel, &r)
 
 	select {
 	case <-ctx.Done():
+		fmt.Println("body:", r.Body)
 		fmt.Println("Gracefully exit")
 		fmt.Println(ctx.Err())
 		return
@@ -27,9 +36,12 @@ func main() {
 	time.Sleep(time.Second * 10)
 }
 
-func f1(t, i int, cancel context.CancelFunc) {
+func f1(t, i int, cancel context.CancelFunc, r *Result) {
 	fmt.Println("processando goroutine:", i, " time: ", t)
 	time.Sleep(time.Second * time.Duration(t))
+	r.Lock()
+	r.Body = `{"msg":"success"}`
+	r.Unlock()
 	cancel()
 	println("cancelando:", i)
 }
