11527816df57158cfe1f8cac72e889aaddb893e5
jefferson otoni <jeff.otoni@s3wf.com.br>
2020-08-31 22:22:53 -0300

    update api

--- api-simple/api-zero/api0.3.go
+++ api-simple/api-zero/api0.3.go
@@ -1,14 +1,54 @@
+// curl -i -XPOST -H "Content-type:application/json" \
+// -d '{"id":3,"name":"carlos lima o foda","location":"Brasil","age":24}' \
+// localhost:3000/api/v1/user
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
+type User struct {
+	ID       int64  `json:"id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Location string `json:"location,omitempty"`
+	Age      int    `json:"age,omitempty"`
+}
+
+type msgError struct {
+	Msg string `json:"msg"`
+}
+
+var msgE msgError
+
 func main() {
-	http.HandleFunc("/ping",
+
+	http.HandleFunc("/api/v1/user",
 		func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("pong"))
+			var p User
+			err := json.NewDecoder(r.Body).Decode(&p)
+			if err != nil {
+				msgE.Msg = "Error body!"
+				w.WriteHeader(400)
+				json.NewEncoder(w).Encode(&msgE)
+				return
+			}
+
+			if p.Age < 0 {
+				msgE.Msg = "Campo Age obrigatorio!"
+				w.WriteHeader(400)
+				json.NewEncoder(w).Encode(&msgE)
+				return
+			}
+			//fmt.Println(p)
+			//p.Ip = c.RemoteAddr
+			time.Sleep(time.Millisecond * 50)
+			msgE.Msg = "success"
+			w.WriteHeader(200)
+			json.NewEncoder(w).Encode(&msgE)
 		})
+
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
