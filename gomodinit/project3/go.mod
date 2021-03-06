3c3734fb449be992ff47f2b7aedbbb8178bf3d02
jeffeotoni <jeff.otoni@gmail.com>
2021-03-06 18:34:47 -0300

    example repo private github

---
+++ gomodinit/project3/go.mod
@@ -0,0 +1,5 @@
+module project3
+
+go 1.16
+
+require github.com/jeffotoni/gcolorfake v0.0.1
---
+++ gomodinit/project3/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/jeffotoni/gcolorfake"
+
+func main(){
+
+	println("test private pkg")
+	println(gcolorfake.YellowCor("testando fake private..."))
+}
