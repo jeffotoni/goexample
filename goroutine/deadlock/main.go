bf51a07a1f0f4f83ddee91e99f0cc9cdef69b2d2
jeffeotoni <jeff.otoni@gmail.com>
2021-07-09 14:32:29 -0300

    deadloack

--- goroutine/deadlock/main.go
+++ goroutine/deadlock/main.go
@@ -1,17 +1,16 @@
 package main
 
-import "time"
-
 func main() {
 	messages := make(chan string)
 
 	// A go routine that sends to a channel
-	//go func() {
-		messages <- "I am sending a message."
-	//}()
+	go func() {
+		//messages <- "I am sending a message."
+	}()
 
 	// Main routine has no receiver.
 	// No compilation or runtime error.
 	// Why doesn't it deadlock?
-	time.Sleep(time.Second)
+	//time.Sleep(time.Second)
+	<- messages
 }
