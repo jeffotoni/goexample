0be1fa892d729e499a0bbbcbe5e5196e2eb62c6f
jeffeotoni <jeff.otoni@gmail.com>
2021-10-31 15:16:58 -0300

    fiber api

--- api-simple/fiber/main.go
+++ api-simple/fiber/main.go
@@ -2,12 +2,29 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+type userPost struct {
+  Name string `json:"name"`
+  Cpf string  `json:"cpf"`
+  Year int `json:"year"`
+}
+
 func main() {
 	app := fiber.New()
-	app.Get("/hello", Hello)
+  app.Get("/api/v1/ping", Ping)
+  app.Post("/api/v1/user", User)
 	app.Listen(":8080")
 }
 
-func Hello(c *fiber.Ctx) error {
-	return c.Status(200).SendString("hello ok!")
+func Ping(c *fiber.Ctx) error {
+  c.Set("Content-Type", "application/json")
+  return c.Status(200).SendString(`{"msg":"pong"}`)
+}
+
+func User(c *fiber.Ctx) (err error) {
+  var user userPost
+  err = c.BodyParser(&user)
+  if err !=nil{
+    return c.Status(400).SendString(`{"msg":"error parse, json:`+err.Error()+`"}`)
+  }
+  return c.Status(200).JSON(user)
 }
