0cd10e8bee314e1afcda5fc2b54f6330829a9fe1
jeffeotoni <jeff.otoni@gmail.com>
2022-03-19 12:28:49 -0300

    struct generics 1.18

---
+++ go1.18/generics/poc3/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//type MyStructGeneric[T any] struct {
+//	G *MyStructGeneric[T]
+//}
+//
+//func NewMyStructGeneric[T any]() *MyStructGeneric[T] {
+//	return &MyStructGeneric[T]{
+//		G: &MyStructGeneric[T]{},
+//	}
+//}
+
+type MyStructGeneric[T any] struct {
+	field T
+}
+
+func NewMyStructGeneric[T any]() MyStructGeneric[T] {
+	return MyStructGeneric[T]{
+	}
+}
+type User struct {
+	ID   string
+	Name string
+	Cpf  int
+}
+type Company struct {
+	ID   int
+	Phone string
+	CorporateName string
+}
+
+type Covid struct {
+	Week   int
+	Data string
+	County string
+}
+
+type Universities struct {
+	Name   string
+	Acronym string
+	Country string
+}
+
+func main() {
+	var u = &MyStructGeneric[Universities]{}
+	u.field.Name = "Federal do Rio de Janeiro"
+	u.field.Acronym = "UFRJ"
+	u.field.Country = "Brasil"
+	fmt.Println(u)
+	bj, _ := json.Marshal(&u.field)
+	println("json")
+	fmt.Println(string(bj))
+
+	var c  MyStructGeneric[Covid]
+	c.field.Week = 202211
+	c.field.Data = "2022-03-18"
+	c.field.County = "Cruzeiro do Sul"
+	fmt.Println(c)
+	bj, _ = json.Marshal(&c.field)
+	println("json")
+	fmt.Println(string(bj))
+
+	var b = new(MyStructGeneric[User])
+	b.field.ID = "987654321"
+	b.field.Name = "jeffotoni"
+	b.field.Cpf = 39393939393
+	fmt.Println(b)
+	bj, _ = json.Marshal(&b.field)
+	println("json")
+	fmt.Println(string(bj))
+
+	p := NewMyStructGeneric[Company]()
+	p.field.ID = 123456
+	p.field.CorporateName = "GENERIC COMPANY LTDA"
+	p.field.Phone = "3198765433"
+	fmt.Println(p)
+
+	bj, _ = json.Marshal(&p.field)
+	println("json")
+	fmt.Println(string(bj))
+}
