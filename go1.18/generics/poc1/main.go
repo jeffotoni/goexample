ac02e33d1c60ef9f29696832ca0b02c0ea300e3a
jeffeotoni <jeff.otoni@gmail.com>
2022-03-24 12:50:20 -0300

    correction benchmark

--- go1.18/generics/poc1/main.go
+++ go1.18/generics/poc1/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type iMyinterface interface {
 	int | int64 | int32
 	~float64
@@ -10,35 +8,86 @@ type iMyinterface interface {
 	MyMethod()
 }
 
-func NoGenericFuncInts(i []int) {
-	for _, v := range i {
-		fmt.Sprintf("%v", v)
+func NoGenericFuncInts(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
 	}
+	for i, v := range a {
+		if b[i] != v {
+			return false
+		}
+	}
+	return true
 }
 
-func NoGenericFuncStrs(s []string) {
-	for _, v := range s {
-		fmt.Sprintf("%v", v)
+func NoGenericFuncStrs(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
 	}
+	for i, v := range a {
+		if b[i] != v {
+			return false
+		}
+	}
+	return true
 }
 
-func GenericsSlice[T any](s []T) {
-	for _, v := range s {
-		fmt.Sprintf("%v", v)
+func GenericsComparableSlice[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
 	}
+	for i, v := range a {
+		if b[i] != v {
+			return false
+		}
+	}
+	return true
 }
 
-func NoGenericInterface(i interface{}) {
-	switch i.(type) {
+func GenericsAnySlice[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for range a {
+		// list
+	}
+	return true
+}
+
+func NoGenericInterface(a, b interface{}) bool {
+	switch a.(type) {
 	case []int:
-		for _, v := range i.([]int) {
-			fmt.Sprintf("%v", v)
+		aa := a.([]int)
+		bb := b.([]int)
+		if len(aa) != len(bb) {
+			return false
 		}
+
+		for i, v := range a.([]int) {
+			ii := b.([]int)
+			if ii[i] != v {
+				return false
+			}
+		}
+		return true
 	case []string:
-		for _, v := range i.([]string) {
-			fmt.Sprintf("%v", v)
+		aa := a.([]string)
+		bb := b.([]string)
+		if len(aa) != len(bb) {
+			return false
 		}
+
+		for i, v := range a.([]string) {
+			ii := b.([]string)
+			if ii[i] != v {
+				return false
+			}
+		}
+		return true
+		return true
 	}
+
+	return false
 }
 
 var any1 string
@@ -46,18 +95,28 @@ var T string
 
 func main() {
 	println("version...")
-	GenericsSlice([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	println("")
-	GenericsSlice([]string{"a", "b", "c", "d"})
-	println("")
-	GenericsSlice[string]([]string{"a", "b", "c", "d", "j"})
-	println("")
-	GenericsSlice[int]([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-	println("")
-	NoGenericFuncInts([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	println("")
-	NoGenericInterface([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	println("")
-	NoGenericInterface([]string{"a", "b", "c", "d", "j"})
+
+	var t bool
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	b := a
+	t = NoGenericFuncInts(a, b)
+	println(t)
+
+	s1 := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "jeff", "otoni", "go", "C"}
+	s2 := s1
+	t = NoGenericFuncStrs(s1, s2)
+	println(t)
+
+	t = GenericsComparableSlice(a, b)
+	println(t)
+
+	t = GenericsComparableSlice[string](s1, s2)
+	println(t)
+
+	t = NoGenericInterface(a, b)
+	println(t)
+
+	t = NoGenericInterface(s1, s2)
+	println(t)
 
 }
--- go1.18/generics/poc1/main_test.go
+++ go1.18/generics/poc1/main_test.go
@@ -8,70 +8,70 @@ import (
 var tableInt = []struct {
 	ints []int
 }{
-	{ints: []int{1, 2, 34, 4, 5, 6}},
+	{ints: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}},
 }
 
 var tableStr = []struct {
 	strs []string
 }{
-	{strs: []string{"a", "b", "c", "d", "f", "g"}},
+	{strs: []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "jeff", "otoni", "go", "C"}},
 }
 
-func BenchmarkGenericsA(b *testing.B) {
+func BenchmarkNoGenericFuncInts___________(b *testing.B) {
 	for _, v := range tableInt {
 		b.Run(fmt.Sprintf("%s", ""), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				GenericsSlice(v.ints)
+				NoGenericFuncInts(v.ints, v.ints)
 			}
 		})
 	}
 }
 
-func BenchmarkGenericsB(b *testing.B) {
+func BenchmarkNoGenericFuncStrs___________(b *testing.B) {
 	for _, v := range tableStr {
 		b.Run(fmt.Sprintf("%s", ""), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				GenericsSlice(v.strs)
+				NoGenericFuncStrs(v.strs, v.strs)
 			}
 		})
 	}
 }
 
-func BenchmarkGenericsF(b *testing.B) {
+func BenchmarkGenericsComparableSliceInt(b *testing.B) {
 	for _, v := range tableInt {
 		b.Run(fmt.Sprintf("%s", ""), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				NoGenericFuncInts(v.ints)
+				GenericsComparableSlice(v.ints, v.ints)
 			}
 		})
 	}
 }
 
-func BenchmarkGenericsS(b *testing.B) {
+func BenchmarkGenericsComparableSliceStr(b *testing.B) {
 	for _, v := range tableStr {
 		b.Run(fmt.Sprintf("%s", ""), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				NoGenericFuncStrs(v.strs)
+				GenericsComparableSlice(v.strs, v.strs)
 			}
 		})
 	}
 }
 
-func BenchmarkGenericsI(b *testing.B) {
+func BenchmarkNoGenericInterfaceInt(b *testing.B) {
 	for _, v := range tableInt {
 		b.Run(fmt.Sprintf("%s", ""), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				NoGenericInterface(v.ints)
+				NoGenericInterface(v.ints, v.ints)
 			}
 		})
 	}
 }
 
-func BenchmarkGenericsY(b *testing.B) {
+func BenchmarkNoGenericInterfaceStr(b *testing.B) {
 	for _, v := range tableStr {
 		b.Run(fmt.Sprintf("%s", ""), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				NoGenericInterface(v.strs)
+				NoGenericInterface(v.strs, v.strs)
 			}
 		})
 	}
