5f80d8f1df67e2989e3e77f72c2fa5abd4d7ee94
jeffeotoni <jeff.otoni@gmail.com>
2022-06-27 11:44:49 -0300

    update make Benchmark

--- benchmark/make/main_test.go
+++ benchmark/make/main_test.go
@@ -5,8 +5,8 @@ import "testing"
 func BenchmarkMake(b *testing.B) {
   for n := 0; n < b.N; n++ {
     data := make([]int, 0)
-    for k := 0; k < size; k++ {
-      data = append(data, k)
+    for i := 0; i < size; i++ {
+      data = append(data, i)
     }
   }
 }
@@ -16,8 +16,8 @@ const size = 1000000
 func BenchmarkMakeSize(b *testing.B) {
   for n := 0; n < b.N; n++ {
     data := make([]int, 0, size)
-    for k := 0; k < size; k++ {
-      data = append(data, k)
+    for i := 0; i < size; i++ {
+      data = append(data, i)
     }
   }
 }
