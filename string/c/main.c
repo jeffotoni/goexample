f77c5f38a2d3fe0bd926de4a37f8a52952e192f1
jeffotoni <jeff.otoni@gmail.com>
2022-09-01 23:20:41 -0300

    update c string

--- string/c/main.c
+++ string/c/main.c
@@ -4,16 +4,20 @@
 
 #define MAXBUF  (50)
 
-char * message( void )
+char * message( char *s )
 {
     char * msg = (char*) malloc( (MAXBUF + 1) * sizeof(char) );
-    strncpy( msg, "jeffotoni, let's Go!", MAXBUF );
+    //strncpy( msg, "jeffotoni, let's Go!", MAXBUF );
+    strncpy( msg, s, MAXBUF );
     return msg;
 }
 
 int main( void)
 {
-    char * msg = message();
+    //char s[MAXBUF] = "jeffotoni, let's Go!";
+    char *s = "jeffotoni, let's Go!";
+    //strncpy(s, "jeffotoni, let's Go!", MAXBUF);
+    char *msg = message(s);
     printf("%s\n", msg );
     free(msg);
     return 0;
