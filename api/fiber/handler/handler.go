fd131fd2a520249c928f7df24e7097e149117aca
jeffotoni <jeff.otoni@gmail.com>
2022-10-08 11:55:56 -0300

    pkg crypt, handler

---
+++ api/fiber/handler/handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jeffotoni/goexample/api/fiber/crypt"
+)
+
+func Check(c *fiber.Ctx) error {
+	ID, err := crypt.RandomID()
+	if err != nil {
+		return c.Status(400).SendString(`{"msg":"Bad Request RandomID"}`)
+	}
+	c.Set("ID", ID)
+	return c.Status(200).SendString(`{"msg":"ok", "token":"xxxxxxxxxxxxxxxxxx3993"}`)
+}
+
+func Healthz(c *fiber.Ctx) error {
+	ID, err := crypt.RandomID()
+	if err != nil {
+		return c.Status(400).SendString(`{"msg":"Bad Request RandomID"}`)
+	}
+
+	c.Set("ID", ID)
+	c.Set("Content-Type", "application/json")
+	return c.Status(200).SendString("")
+}
