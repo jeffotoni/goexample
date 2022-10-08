02da27b2ba7eeb95f526059f5fc89af1189f57b4
jeffotoni <jeff.otoni@gmail.com>
2022-10-08 11:43:43 -0300

    k6 test stress sleep

--- api/fiber/k6/get.js
+++ api/fiber/k6/get.js
@@ -1,7 +1,9 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
 
 const headers = { 'Content-Type': 'application/json' };
 
 export default function() {
     http.get(`http://localhost:8080/healthz`, { headers: headers });
+    sleep(1);
 }
