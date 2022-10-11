e62148d649c5193893423d7e5918787711ad2842
jeffotoni <jeff.otoni@gmail.com>
2022-10-11 10:51:03 -0300

    update

--- api/standard/main.go
+++ api/standard/main.go
@@ -1,29 +1,32 @@
 package main
 
-import "net/http"
-import "github.com/jeffotoni/goexample/api/standard/crypt"
-import "time"
-import "log"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/jeffotoni/goexample/api/standard/crypt"
+)
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", Healthz)
 	mux.HandleFunc("/auth/check", Check)
 	s := &http.Server{
-	 Addr: "0.0.0.0:8080",
-	 Handler: mux,
-	 ReadTimeout: 10 * time.Second,
-	 WriteTimeout: 10 * time.Second,
-	 MaxHeaderBytes: 1<< 20, // 1MB
+		Addr:           "0.0.0.0:8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20, // 1MB
 	}
-	println("Run Server :8080")
+	println("Run Server 0.0.0.0:8080")
 	log.Fatal(s.ListenAndServe())
 }
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	ID, err := crypt.RandomID()
-	if err!=nil{
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`{"msg":"Bad Request RandomID"}`))
 		return
@@ -35,5 +38,15 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 }
 
 func Check(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	ID, err := crypt.RandomID()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"msg":"Bad Request RandomID"}`))
+		return
+	}
+	w.Header().Set("ID", ID)
+	defer r.Body.Close()
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"msg":"ok", "token":"xxxxxxxxxxxxxxxxxx3993"}`))
 }
---
+++ api/standard/main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
+
+func TestHandlerRabbit(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HandlerRabbit(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func Test_rabbit(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rabbit(tt.args.msg)
+		})
+	}
+}
+
+func TestHealthz(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Healthz(tt.args.w, tt.args.r)
+		})
+	}
+}
