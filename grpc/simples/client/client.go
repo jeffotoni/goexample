78b324bfe36c83cedecb0916fb0d2d353efddf09
jeffotoni <jeff.otoni@gmail.com>
2023-03-25 12:56:58 -0300

    new version grpc simples

---
+++ grpc/simples/client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jeffotoni/goexample/grpc/simples/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// Conectar ao servidor GRPC
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Printf("Não foi possível conectar ao servidor: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	client := proto.NewClienteServiceClient(conn)
+
+	req := &proto.PostRequest{
+		ID:   12345678,
+		Nome: "Produto CHAT/GPT-4",
+	}
+
+	res, err := client.CreatePost(context.Background(), req)
+	if err != nil {
+		log.Printf("Não foi possível chamar o serviço: %v", err)
+		return
+	}
+
+	fmt.Println(res)
+}
---
+++ grpc/simples/server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pro "github.com/jeffotoni/goexample/grpc/simples/proto"
+)
+
+type postServiceServer struct{}
+
+func (s *postServiceServer) CreatePost(ctx context.Context, in *pro.PostRequest) (*pro.PostResponse, error) {
+
+	fmt.Println("Capturando dados:", in)
+	return &pro.PostResponse{Status: 201, Msg: fmt.Sprintf("Criando Produto: %s", in.Id)}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Printf("failed to listen: %v", err)
+		return
+	}
+
+	s := grpc.NewServer()
+	pro.RegisterPostServiceServer(s, &postServiceServer{})
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Printf("failed to serve: %v", err)
+		return
+	}
+}
