2e6f427c4f85c610102300e642df027858b5ed0e
jeffotoni <jeff.otoni@gmail.com>
2025-06-07 20:13:10 -0300

    docs: worker pool

--- goroutine/worker.go
+++ goroutine/worker.go
@@ -3,7 +3,7 @@
 *
 * @package     main
 * @author      @jeffotoni
-* @size        01/08/2017
+* @sice        01/08/2017
 *
  */
 
@@ -23,46 +23,41 @@ import (
 // results on `results`. We'll sleep a second per job to
 // simulate an expensive task.
 func worker(id int, jobs <-chan int, results chan<- int) {
-
 	for j := range jobs {
-
-		fmt.Println("worker", id, "started  job", j)
-
+		fmt.Printf("👷 Worker %d → started job %d\n", id, j)
 		time.Sleep(time.Second)
-
-		fmt.Println("worker", id, "finished job", j)
+		fmt.Printf("✅ Worker %d → finished job %d\n", id, j)
 		results <- j * 2
 	}
 }
 
 func main() {
 
+	numJobs := 5
+	numWorkers := 3
+
 	// In order to use our pool of workers we need to send
 	// them work and collect their results. We make 2
 	// channels for this.
-	jobs := make(chan int, 100)
-	results := make(chan int, 100)
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numWorkers)
 
 	// This starts up 3 workers, initially blocked
 	// because there are no jobs yet.
-	for w := 1; w <= 3; w++ {
-
+	for w := 1; w <= numWorkers; w++ {
 		go worker(w, jobs, results)
 	}
 
 	// Here we send 5 `jobs` and then `close` that
 	// channel to indicate that's all the work we have.
-	for j := 1; j <= 5; j++ {
-
+	for j := 1; j <= numJobs; j++ {
 		jobs <- j
-
 	}
-
 	close(jobs)
 
 	// Finally we collect all the results of the work.
-	for a := 1; a <= 5; a++ {
-
-		<-results
+	for a := 1; a <= numJobs; a++ {
+		result := <-results
+		fmt.Println("🔁 Result received:", result)
 	}
 }
